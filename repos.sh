96eae471f94b702b4305418dbc2825a63628af6d
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-04 15:38:38 +0200

    Add files via upload

---
+++ repos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "### Adding Tailscale repository..."
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list
+
+echo "### Adding Docker repository..."
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
+| tee /etc/apt/sources.list.d/docker.list
+
+echo "### All repositories added successfully."
