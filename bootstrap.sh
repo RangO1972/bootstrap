ce46cb940f39172f7f49560f301a8b6a559f0681
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-08 09:11:31 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@ set -e
 
 WORKDIR="/opt/stradcs-bootstrap/scripts"
 
+echo "[01-init-vars] Rendo eseguibili tutti gli script..."
+find "$WORKDIR" -type f -name "*.sh" -exec chmod +x {} \;
+
 echo "### Running bootstrap scripts..."
 for script in $(ls $WORKDIR | sort); do
    echo "### Executing $script..."
