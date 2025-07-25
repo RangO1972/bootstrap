195f69a45ca7819fe39ba6e278f7fcf380950fe9
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-25 14:13:05 +0200

    Update 10-setup-essential.sh

--- scripts/10-setup-essential.sh
+++ scripts/10-setup-essential.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "[10-setup-essential] Aggiornamento pacchetti..."
-apt-get update
-
-echo "[10-setup-essential] Installazione pacchetti minimi essenziali..."
-apt-get install -y \
-    curl \
-    ca-certificates \
-    gnupg \
-    lsb-release \
-    git \
-    sudo \
-    systemd-resolved 
-
 echo "[10-setup-essential] Abilitazione e attivazione systemd-networkd e resolved..."
 systemctl enable systemd-networkd.service
 systemctl enable systemd-resolved.service
