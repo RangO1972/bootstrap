3beaaec90927302b121215b0146676d91bd82970
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-26 11:04:53 +0200

    Update 55-create-user.sh

--- scripts/55-create-user.sh
+++ scripts/55-create-user.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+TAG="\033[1;37m[$(basename "$0" .sh)]\033[0m"
 
 USER="stra"
 
-echo "### Creating user $USER..."
+echo "$TAG - Creating user $USER..."
 
 if id "$USER" >/dev/null 2>&1; then
   echo "User $USER already exists."
@@ -13,8 +14,8 @@ else
   echo "  passwd $USER"
 fi
 
-echo "### Adding $USER to sudo group..."
+echo "$TAG - Adding $USER to sudo group..."
 usermod -aG sudo "$USER"
 
-echo "### Adding $USER to docker group..."
+echo "$TAG - Adding $USER to docker group..."
 usermod -aG docker "$USER"
