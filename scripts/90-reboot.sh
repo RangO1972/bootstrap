d02db563242f98c00f41af42b21120e1df37b153
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-26 11:06:15 +0200

    Update 90-reboot.sh

--- scripts/90-reboot.sh
+++ scripts/90-reboot.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
+TAG="\033[1;37m[$(basename "$0" .sh)]\033[0m"
 
-echo "### Rebooting in 5 seconds..."
-sleep 5
+echo "$TAG - Rebooting in 5 seconds..."
+sleep 1
+echo "$TAG - Rebooting in 4 seconds..."
+sleep 1
+echo "$TAG - Rebooting in 3 seconds..."
+sleep 1
+echo "$TAG - Rebooting in 2 seconds..."
+sleep 1
+echo "$TAG - Rebooting in 1 seconds..."
+sleep 1
+echo "$TAG - Rebooting..."
 reboot
