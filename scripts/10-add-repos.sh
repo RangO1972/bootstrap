cced039522dd534c5b3db89133a34124d5d17b8b
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-26 11:00:15 +0200

    Update 10-add-repos.sh

--- scripts/10-add-repos.sh
+++ scripts/10-add-repos.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
+TAG="\033[1;37m[$(basename "$0" .sh)]\033[0m"
 
-echo "[20-add-repos] Aggiunta repository Tailscale e Docker..."
+echo "$TAG - Aggiunta repository Tailscale e Docker..."
 
 # Install tools necessari
 apt-get update
@@ -12,12 +13,12 @@ echo "deb http://deb.debian.org/debian bookworm-backports main contrib non-free 
     > /etc/apt/sources.list.d/backports.list
     
 # === TAILSCALE ===
-echo "[20-add-repos] Aggiungo repository Tailscale..."
+echo "$TAG - Aggiungo repository Tailscale..."
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list >/dev/null
 
 # === DOCKER ===
-echo "[20-add-repos] Aggiungo repository Docker..."
+echo "$TAG - Aggiungo repository Docker..."
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -28,10 +29,10 @@ $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update finale
-echo "[20-add-repos] Eseguo apt update finale..."
+echo "$TAG - Eseguo apt update finale..."
 apt-get update
 
-echo "[20-add-repos] Installo kernel e header da backports..."
+echo "$TAG - Installo kernel e header da backports..."
 apt-get install -y -t bookworm-backports linux-image-amd64 linux-headers-amd64
 
-echo "[20-add-repos] Repositories aggiunti correttamente."
+echo "$TAG - Repositories aggiunti correttamente."
