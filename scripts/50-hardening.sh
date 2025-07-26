483e5b7e5ecc2c61339479f20e2e8f5fdb20d6ad
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-26 11:04:09 +0200

    Update 50-hardening.sh

--- scripts/50-hardening.sh
+++ scripts/50-hardening.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
+TAG="\033[1;37m[$(basename "$0" .sh)]\033[0m"
 
-echo "### Disabling root SSH login..."
+echo "$TAG - Disabling root SSH login..."
 sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 systemctl restart ssh || true
 
-echo "### Removing obsolete packages..."
+echo "$TAG - Removing obsolete packages..."
 apt-get remove -y ifupdown ifenslave resolvconf rpcbind nfs-common avahi-daemon at || true
 
-echo "### Linking resolv.conf to systemd-resolved..."
+echo "$TAG - Linking resolv.conf to systemd-resolved..."
 ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
-echo "### Enabling systemd services..."
+echo "$TAG - Enabling systemd services..."
 systemctl enable systemd-networkd
 systemctl enable systemd-resolved
