b5ea4c0444f67ad5b13c5745684b8300dc4698f2
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-26 11:06:58 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+TAG="\033[1;37m[$(basename "$0" .sh)]\033[0m"
 
 # Inizializzazione variabili globali
 REPO_URL="https://github.com/RangO1972/bootstrap.git"
@@ -15,13 +16,13 @@ apt update && apt install -y git
 # Clono repository
 git clone "$REPO_URL" "$WORKDIR"
 
-echo "[01-init-vars] Imposto permessi nella cartella di lavoro: $WORKDIR"
+echo "$TAG - Imposto permessi nella cartella di lavoro: $WORKDIR"
 chown -R "$TARGET_USER:$TARGET_USER" "$WORKDIR"
 
-echo "[01-init-vars] Rendo eseguibili tutti gli script..."
+echo "$TAG - Rendo eseguibili tutti gli script..."
 find "$SCRIPTSDIR" -type f -name "*.sh" -exec chmod +x {} \;
 
-echo "### Running bootstrap scripts..."
+echo "$TAG - Running bootstrap scripts..."
 for script in $(ls $SCRIPTSDIR | sort); do
    echo "### Executing $script..."
    bash "$SCRIPTSDIR/$script"
