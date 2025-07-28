13d59c832022ea8e77822deb8f6a8f8e7d54e241
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-28 19:35:46 +0200

    Update common.sh

--- lib/common.sh
+++ lib/common.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Logging con livelli
+: "${WORKDIR:=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)}"
+
 log() {
     local level="$1"
     local message="$2"
@@ -12,18 +13,10 @@ log() {
     caller_file="${caller_file%.sh}"
 
     case "$level" in
-        info)
-            color="\033[1;32m[INFO ]\033[0m"
-            ;;
-        warn)
-            color="\033[1;33m[WARN ]\033[0m"
-            ;;
-        error)
-            color="\033[1;31m[ERROR]\033[0m"
-            ;;
-        *)
-            color="\033[1;37m[LOG  ]\033[0m"
-            ;;
+        info)  color="\033[1;32m[INFO ]\033[0m" ;;
+        warn)  color="\033[1;33m[WARN ]\033[0m" ;;
+        error) color="\033[1;31m[ERROR]\033[0m" ;;
+        *)     color="\033[1;37m[LOG  ]\033[0m" ;;
     esac
 
     echo -e "\033[1;37m[${caller_file}]\033[0m $timestamp $color $message"
