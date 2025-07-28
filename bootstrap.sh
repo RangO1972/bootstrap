4f6168e6905f1ca934b1c885246826c6fa9a5cd0
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-28 19:35:01 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
 set -e
-TAG="\033[1;37m[$(basename "$0" .sh)]\033[0m"
 
-# Inizializzazione variabili globali
-REPO_URL="https://github.com/RangO1972/bootstrap.git"
+# Imposta WORKDIR assoluto (fisso)
 export WORKDIR="/opt/stradcs-bootstrap"
+
+# Altre variabili globali
+export REPO_URL="https://github.com/RangO1972/bootstrap.git"
 export INTERFACE_DIR="$WORKDIR/interfaces"
 export TARGET_USER="stra"
 export NETWORKD_DIR="/etc/systemd/network"
 export SCRIPTSDIR="$WORKDIR/scripts"
 
-# Installo git
-apt update && apt install -y git
+# Includi funzioni comuni
+source "$WORKDIR/lib/common.sh"
 
-# Clono repository
-git clone "$REPO_URL" "$WORKDIR"
+log info "Inizio bootstrap da $WORKDIR"
 
-echo "$TAG - Imposto permessi nella cartella di lavoro: $WORKDIR"
+# Installa Git se non esiste
+if ! command -v git &>/dev/null; then
+    log info "Git non trovato. Installo git..."
+    apt update && apt install -y git
+else
+    log info "Git già installato"
+fi
+
+# Clona repo se necessario
+if [ ! -d "$WORKDIR/.git" ]; then
+    log info "Clono il repository da $REPO_URL"
+    git clone "$REPO_URL" "$WORKDIR"
+else
+    log warn "Repository già presente in $WORKDIR. Salto il clone."
+fi
+
+# Permessi e chmod
+log info "Imposto permessi nella cartella di lavoro: $WORKDIR"
 chown -R "$TARGET_USER:$TARGET_USER" "$WORKDIR"
 
-echo "$TAG - Rendo eseguibili tutti gli script..."
+log info "Rendo eseguibili tutti gli script in $SCRIPTSDIR"
 find "$SCRIPTSDIR" -type f -name "*.sh" -exec chmod +x {} \;
 
-echo "$TAG - Running bootstrap scripts..."
-for script in $(ls $SCRIPTSDIR | sort); do
-   echo "### Executing $script..."
-   bash "$SCRIPTSDIR/$script"
+# Esecuzione ordinata degli script
+log info "Avvio degli script in $SCRIPTSDIR"
+for script in $(ls "$SCRIPTSDIR" | sort); do
+    log info "Eseguo $script..."
+    bash "$SCRIPTSDIR/$script"
 done
+
+log info "Bootstrap completato"
