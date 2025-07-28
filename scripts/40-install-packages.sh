faa564b1e2ba8958f42be6a865062ebba95894eb
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-28 19:41:49 +0200

    Update 40-install-packages.sh

--- scripts/40-install-packages.sh
+++ scripts/40-install-packages.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
-TAG="\033[1;37m[$(basename "$0" .sh)]\033[0m"
 
-echo "$TAG -Updating package lists..."
+: "${WORKDIR:=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)}"
+source "$WORKDIR/lib/common.sh"
+
+log info "Updating package lists..."
 apt-get update
 
-echo "$TAG -Installo kernel e header da backports..."
+log info "Installo kernel e header da backports..."
 apt-get install -y -t bookworm-backports linux-image-amd64 linux-headers-amd64
 
-echo "$TAG -### Installing packages..."
+log info "### Installing packages..."
 apt-get install -y $(cat /opt/stradcs-bootstrap/configs/packages.txt)
