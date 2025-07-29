b2271eefcf55ab21d33a351f06afc020ca8dec8e
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-29 23:00:59 +0200

    Update 41-tailscale.sh

--- scripts/41-tailscale.sh
+++ scripts/41-tailscale.sh
@@ -7,6 +7,8 @@ source "$WORKDIR/lib/common.sh"
 
 # === TAILSCALE ===
 
+apt-get install -y curl
+
 log info "Add Tailscale repository..."
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list >/dev/null
