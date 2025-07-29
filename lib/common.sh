1c2d0a85990eb626b920f5f3b5eaba18d8348288
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-30 00:43:23 +0200

    Update common.sh

--- lib/common.sh
+++ lib/common.sh
@@ -6,7 +6,7 @@
 LOGDIR="$WORKDIR/logs"
 mkdir -p "$LOGDIR"
 
-# Imposta il file di log con nome del giorno
+# Imposta il file di log con nome del giorno (es: 2025-07-30.log)
 LOGFILE="$LOGDIR/$(date +%F).log"
 
 log() {
@@ -14,32 +14,32 @@ log() {
     local message="$2"
     local timestamp
     local caller_file
+    local journal_level
 
     timestamp=$(date "+%Y-%m-%d %H:%M:%S")
     caller_file=$(basename "${BASH_SOURCE[1]:-${BASH_SOURCE[0]}}")
     caller_file="${caller_file%.sh}"
 
     case "$level" in
-        info)  color="\033[1;32m[INFO ]\033[0m" ;;
-        warn)  color="\033[1;33m[WARN ]\033[0m" ;;
-        error) color="\033[1;31m[ERROR]\033[0m" ;;
-        *)     color="\033[1;37m[LOG  ]\033[0m" ;;
+        info)    color="\033[1;32m[INFO ]\033[0m"; journal_level="info" ;;
+        warn)    color="\033[1;33m[WARN ]\033[0m"; journal_level="warning" ;;
+        error)   color="\033[1;31m[ERROR]\033[0m"; journal_level="err" ;;
+        debug)   color="\033[1;34m[DEBUG]\033[0m"; journal_level="debug" ;;
+        notice)  color="\033[1;36m[NOTICE]\033[0m"; journal_level="notice" ;;
+        *)       color="\033[1;37m[LOG  ]\033[0m"; journal_level="info" ;;
     esac
 
+    # Formato stampato e loggato
     formatted="$timestamp [$level] [$caller_file] $message"
 
-    # Stampa a video
-    echo -e "\033[1;37m[${caller_file}]\033[0m $timestamp $color $message"
+    # Output a video
+    echo -e "$timestamp $color \033[1;37m[${caller_file}]\033[0m $message"
 
-
-    # Scrive nel file giornaliero
+    # Scrive anche su file
     echo "$formatted" >> "$LOGFILE"
 
-    # Scrive anche nel journal se disponibile
+    # Invia a systemd-journal (se disponibile)
     if command -v systemd-cat &>/dev/null; then
-        formatted2="[$level] [$caller_file] $message"
-
-        echo "$formatted2" | systemd-cat -t "stradcs-bootstrap" -p "${level,,}"
+        echo "$formatted" | systemd-cat -t "stradcs-bootstrap" -p "$journal_level"
     fi
-
 }
