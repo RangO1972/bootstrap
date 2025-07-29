9139bc999b5b33f0cb16d62fd889341f5e034f42
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-29 19:08:12 +0200

    Create 41-tailscale.sh

---
+++ scripts/41-tailscale.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+: "${WORKDIR:=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)}"
+source "$WORKDIR/lib/common.sh"
+
+
+# === TAILSCALE ===
+
+log info "Add Tailscale repository..."
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list >/dev/null
+
+apt-get update
+apt-get install tailscale -y
+
