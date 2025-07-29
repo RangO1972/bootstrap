784d4739dc3ff8c62e013dac14ab6e053de2c4c5
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-07-30 00:39:49 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,7 @@ if ! command -v git &>/dev/null; then
     echo "Git non trovato. Installo git..."
     apt update && apt install -y git
 else
-    log warn "Git già installato"
+    echo "Git già installato"
 fi
 
 # Clona repo se necessario
