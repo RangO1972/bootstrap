84f47b66225ab0918fccdc06deab50bc4ba4cfd8
Stefano Ranghetto <stefanoranghetto@hotmail.com>
2025-09-23 09:53:21 +0200

    Update 40-install-packages.sh

--- scripts/40-install-packages.sh
+++ scripts/40-install-packages.sh
@@ -7,9 +7,6 @@ source "$WORKDIR/lib/common.sh"
 log info "Updating package lists..."
 apt-get update
 
-log info "Installing kernel & header..."
-apt-get install -y -t bookworm-backports linux-image-amd64 linux-headers-amd64
-
 log info "Installing packages..."
 apt-get install -y $(cat /opt/stradcs-bootstrap/configs/packages.txt)
 
